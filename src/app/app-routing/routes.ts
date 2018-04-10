eaffef98ecb46ae1583a228382500dd2bed401ac
davidblj <david.blj95@gmail.com>
2018-04-10 10:21:20 -0500

    configuracion: iniciar nueva jerarquia de modulos [skip ci]

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,13 +1,5 @@
 import { Routes} from '@angular/router';
 
-import { HomeComponent} from '../pages/guest-page/home/home.component';
-import { CompanyDashboardComponent} from '../pages/company-page/company-dashboard/company-dashboard.component';
-import { CompanyAuthGuard} from './guards/companyAuthGuard';
-import { DashboardTableComponent } from '../pages/company-page/dashboard-table/dashboard-table.component';
-import { CompanyDetailsComponent } from '../pages/guest-page/company-details/company-details.component';
-import { PickSidesComponent } from '../pages/guest-page/pick-sides/pick-sides.component';
-import { ApplicantDashboardComponent } from '../pages/applicant-page/applicant-dashboard/applicant-dashboard.component';
-
 import { RegisterCardComponent } from '../feature/register-company/components/register-card/register-card.component';
 import { RegisterCandidateEntryPointComponent } from '../feature/register-candidate/components/register-candidate-entry-point/register-candidate-entry-point.component';
 import { SearchDashboardComponent } from '../feature/search/components/search-dashboard/search-dashboard.component';
@@ -19,19 +11,5 @@ export const routes: Routes = [
   {path: 'devs', component: SearchDashboardComponent},
   {path: 'devc', component: RegisterCandidateEntryPointComponent},
   {path: 'devl', component: LogInComponent},
-
-  // -----------------------------------------------------------------
-
-  {path: 'search', component: ApplicantDashboardComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'pick-sides',
-    children: [
-      {path: '', component: PickSidesComponent},
-    ]
-  },
-  {path: 'company-details/:companyName', component: CompanyDetailsComponent},
-  {path: 'dashboard', component: CompanyDashboardComponent, canActivate: [CompanyAuthGuard], children: [
-    {path: '', component: DashboardTableComponent },
-  ]},
-  {path: '**', redirectTo: '/home', pathMatch: 'full'}
+  // {path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,102 +1,20 @@
-// librerias
 
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RestangularModule} from 'ngx-restangular';
 import { AppRoutingModule} from './app-routing/app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-// conf: modules
-
 import { FeatureModule } from './feature/feature.module';
-import { PopoverModule } from 'ngx-bootstrap';
-
-// componentes
-
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/guest-page/home/home.component';
-import { HeaderComponent } from './pages/guest-page/header/header.component';
-import { CompanyDashboardComponent } from './pages/company-page/company-dashboard/company-dashboard.component';
-import { LoginComponent } from './pages/guest-page/login/login.component';
-import { JobFormComponent } from './pages/company-page/job-form/job-form.component';
-import { DashboardTableComponent } from './pages/company-page/dashboard-table/dashboard-table.component';
-import { PickSidesComponent } from './pages/guest-page/pick-sides/pick-sides.component';
-import { ApplicantDashboardComponent } from './pages/applicant-page/applicant-dashboard/applicant-dashboard.component';
-import { CompanyDetailsComponent } from './pages/guest-page/company-details/company-details.component';
-import { JobComponent } from './pages/company-page/job/job.component';
-
-// servicios
-
-import { CompanyService } from './services/organizacion/company.service';
-import { LoginService } from './services/organizacion/login.service';
-import { RegisterJobService } from './services/organizacion/register-job.service';
-import { JobListService } from './services/organizacion/job-list.service';
-import { SearchService } from './services/guest/search.service';
-import { ApplyService } from './services/applicant/apply.service';
-import { JobService } from './services/organizacion/job.service';
-
-// values
-
-import { baseURL } from './shared/baseurl';
-
-// config files
-
-import { RestangularConfigFactory } from './shared/restConfig';
-import { CompanyAuthGuard } from './app-routing/guards/companyAuthGuard';
-import { JobDetailsComponent } from './pages/applicant-page/job-details/job-details.component';
-
-// modules
-
-import { SharedModule } from './pages/shared/shared.module';
-import { ApplicantModule } from './pages/applicant-page/applicant.module';
-
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    LoginComponent,
-    CompanyDashboardComponent,
-    JobFormComponent,
-    DashboardTableComponent,
-    CompanyDetailsComponent,
-    PickSidesComponent,
-    ApplicantDashboardComponent,
-    JobDetailsComponent,
-    JobComponent
   ],
   imports: [
-
-    // refactoring modules
     FeatureModule,
-
-    // legacy modules
-    BrowserModule,
-    RestangularModule.forRoot(RestangularConfigFactory),
-    NgbModule.forRoot(),
-    PopoverModule.forRoot(),
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    SharedModule,
-    ApplicantModule
-  ],
-  entryComponents: [
-    LoginComponent,
-    JobFormComponent
+    BrowserModule,
   ],
   providers: [
-    CompanyService,
-    LoginService,
-    RegisterJobService,
-    CompanyAuthGuard,
-    JobListService,
-    SearchService,
-    ApplyService,
-    JobService,
-    { provide: 'BaseURL', useValue: baseURL }
   ],
   bootstrap: [AppComponent]
 })
