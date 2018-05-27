ada0fecf7e3aad13381de0bd6c840f41164e93a1
davidblj <david.blj95@gmail.com>
2018-05-27 18:27:55 -0500

    agregar seccion 'welcome'

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeWelcomeComponent } from './components/home-welcome/home-welcome.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, HomeWelcomeComponent]
 })
 export class HomeModule { }
