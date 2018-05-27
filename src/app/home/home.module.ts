8d3eddc0df2a362e986fcc21df10db87e52c595f
davidblj <david.blj95@gmail.com>
2018-05-27 18:44:14 -0500

    agregar seccion 'info'

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeWelcomeComponent } from './components/home-welcome/home-welcome.component';
+import { HomeInfoComponent } from './components/home-info/home-info.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule
   ],
-  declarations: [HomeComponent, HomeWelcomeComponent]
+  declarations: [HomeComponent, HomeWelcomeComponent, HomeInfoComponent]
 })
 export class HomeModule { }
